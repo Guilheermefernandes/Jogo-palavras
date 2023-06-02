add2a9bfa6cb49eeeec2b1ee8d398ceb403dea0d
Guilherme Fernandes <80402348+Guilheermefernandes@users.noreply.github.com>
2023-06-02 15:44:43 -0300

    Update buyTip.js

--- buyTip.js
+++ buyTip.js
@@ -1,16 +1,19 @@
 document.querySelector('.tipMoney').addEventListener('click', () => {
     let elementMsg = document.querySelector('.msg');
-    let moneyStorage = parseInt(localStorage.getItem('money'));
-    if(moneyStorage >= 20){
-        let calc = moneyStorage - 20;
-        localStorage.setItem('money', calc);
-        attMoney();
-        tipReset();
-        playerRespect(15);
+    if(clickButton !== true){
+        let moneyStorage = parseInt(localStorage.getItem('money'));
+        if(moneyStorage >= 20){
+            let calc = moneyStorage - 20;
+            localStorage.setItem('money', calc);
+            attMoney();
+            tipReset();
+            playerRespect(15);
+        }else{
+            elementMsg.innerHTML = `Você não possui cents o suficiente: Possui apenas ${moneyStorage} cents.`
+        }
     }else{
-        elementMsg.innerHTML = `Você não possui cents o suficiente: Possui apenas ${moneyStorage} cents.`
+        elementMsg.innerHTML = `Você já possui uma dica, use ela antes de comprar outra!`
     }
-
 });
 
 function tipReset(){
